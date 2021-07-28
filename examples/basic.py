3621d34bcbc5dc58cfde430f72abdaf776a0926c
Yichen Xu <yichen.x@outlook.com>
2021-07-28 21:23:14 +0800

    Add examples

---
+++ examples/basic.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from happy_config import ConfigLoader
+from happy_config.typechecking import show_type
+
+
+@dataclass
+class Bar:
+    s: str
+
+
+@dataclass
+class Foo:
+    x: float
+    y: int
+    bar: Bar
+
+
+@dataclass
+class Config:
+    foo: Foo
+    i: int = 0
+
+
+if __name__ == '__main__':
+    loader = ConfigLoader(model=Config, default_param_path='basic.json')
+    print(show_type(loader.config_type))
+    config = loader()
+    print(config)
