d4d959c67eb6ca0605d42a8f35e916d3a536f71f
Yichen Xu <yichen.x@outlook.com>
2021-07-28 21:13:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="happy_config",
-    version="0.0.1",
+    version="0.0.2",
     author="Yichen Xu",
     author_email="yichen.x@outlook.com",
     description="Managing ML experiment configuation with joy.",
